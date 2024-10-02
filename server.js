f02e94396cae990099563d693099980b7fd333df
thefumiu <Uysalfurkanmirac@icloud.com>
2024-10-02 17:41:18 +0300

    Hotfix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
 const PORT = 8083;
 
 const app = express();
+// Middleware to parse JSON
+app.use(express.json());
+app.use(express.static("files"));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.send("Hello World");
