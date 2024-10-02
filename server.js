c8d6900c07459028b2a8b6a0b85a7cfb10d93c7e
thefumiu <Uysalfurkanmirac@icloud.com>
2024-10-02 17:45:55 +0300

    Hotfix

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
+
+// SETUP - global variables
 const PORT = 8083;
 
+// SETUP - express
 const app = express();
-// Middleware to parse JSON
 app.use(express.json());
 app.use(express.static("files"));
 app.set('view engine', 'ejs');
 
+// METHODS
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("home.ejs");
+});
+app.get("/target", (req, res) => {
+    res.render("target.ejs");
 });
 
+// LAUNCH
 app.listen(PORT, () => {
     console.log('Server running at http://localhost:' + PORT);
 });
